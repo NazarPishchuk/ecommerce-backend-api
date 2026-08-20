1f7ec359c83a440095daab768202a49f86574aa0
NazarPishchuk <a12534317@unet.univie.ac.at>
2026-08-20 13:08:54 +0300

    feat(categories): add category CRUD API

---
+++ ECommerce.Application/DTOs/Categories/CreateCategoryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.Application.DTOs.Categories;
+
+public class CreateCategoryDto
+{
+    public string Name { get; set; } = null!;
+}
---
+++ ECommerce.Application/DTOs/Categories/UpdateCategoryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.Application.DTOs.Categories;
+
+public class UpdateCategoryDto
+{
+    public string Name { get; set; } = null!;
+}
